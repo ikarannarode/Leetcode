e93098239263f47135b5798cda47fe5e79163fa9
Karan Narode <63549941+ikarannarode@users.noreply.github.com>
2025-12-23 11:00:39 +0530

    Time: 0 ms (100%), Space: 57.9 MB (25.71%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -1,13 +1,23 @@
-function reverseList(head){
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
     let prev=null;
-    let curr=head;
-    let next=null;
-    while(curr!=null){
-        next=curr.next;
-        curr.next=prev;
-        prev=curr;
-        curr=next;
+    let current=head;
+    while(current!=null){
+        let next=current.next
+        current.next=prev
+        prev=current;
+        current=next
+
     }
     return prev;
-
-}
+};
